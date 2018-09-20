d0bfdbbbb6184fbfb1d8be1f0263a002258ad1b2
dairoot <623815825@qq.com>
2018-09-20 10:46:19 +0800

    去除图片保存

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from PIL import Image
 from utils import denoise_img, str_turn_num
 
 folder = 'zfxfzb-code-data/img/'
-imgs_folder = 'train_img/'
-single_folder = folder + 'train_img_split/%s_%s.png'
+imgs_folder = folder+'train_img/'
+single_img_name = folder + 'single_imgs/%s_%s.png'
 
 
 class ProcePhoto():
@@ -24,24 +24,25 @@ class ProcePhoto():
         piece = (x_size-22) / 8
         centers = [4+piece*(2*i+1) for i in range(4)]
         for i, center in enumerate(centers):
-            # 存储分割后的图片
-            single_img_path = single_folder % (self.img_name[i], self.img_name)
-            self.im.crop((center-(piece+2), 1, center+(piece+2), y_size)
-                         ).save(single_img_path)
 
-    def save_img_data(self):
+            single_img = self.im.crop(
+                (center-(piece+2), 1, center+(piece+2), y_size))
+            # 存储分割后的图片
+            # single_img_path = single_img_name % (self.img_name[i], self.img_name)
+            # single_img.save(single_img_path)
+            # 存储分割后的图片像素点
+            self.save_img_data(single_img, self.img_name[i])
+
+    def save_img_data(self, img, c):
         ''' 存储分割后的图片像素点 '''
-        for i in range(4):
-            single_img_path = single_folder % (self.img_name[i], self.img_name)
-            img = Image.open(single_img_path)
-            for h in range(0, img.size[1]):
-                for w in range(0, img.size[0]):
-                    pixel = img.getpixel((w, h))
-                    if pixel == 255:
-                        print >> self.data_file_handle, 1,
-                    else:
-                        print >> self.data_file_handle, 0,
-            print >> self.data_file_handle, str_turn_num(self.img_name[i]), ''
+        for h in range(0, img.size[1]):
+            for w in range(0, img.size[0]):
+                pixel = img.getpixel((w, h))
+                if pixel == 255:
+                    print >> self.data_file_handle, 1,
+                else:
+                    print >> self.data_file_handle, 0,
+        print >> self.data_file_handle, str_turn_num(c), ''
 
 
 if __name__ == "__main__":
@@ -50,4 +51,3 @@ if __name__ == "__main__":
         for img_name in img_files:
             im = ProcePhoto(imgs_folder, img_name, data_file)
             im.split_img()
-            im.save_img_data()
